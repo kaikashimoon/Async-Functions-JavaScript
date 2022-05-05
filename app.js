34f24173a33671a1043e07895d807ddb6ef2d4d3
kai <leonc4667@gmail.com>
2022-05-05 16:32:40 -0400

    async await example

--- app.js
+++ app.js
@@ -1,36 +1,4 @@
-let stocks  = {
-    fruits : ['strawberry', 'grapes', 'banana', 'apple'],
-    liquid : ['water', 'ice'],
-    holder : ['cone', 'cup', 'stick'],
-    toppings : ['chocolate', 'peanuts'],
-}
+console.log('Hello, nice coding :D!')
 
 
-let isShopOpen = Boolean = false
 
-let toppingsChoice = () => {
-    
-    return new Promise((resolve, reject) =>{
-        setTimeout(() => {
-            resolve(
-                console.log('Wich topping would you like?')
-            )
-        }, 2000);
-    })
-}
-
-async function kitchen() {
-    console.log('A')
-    console.log('B')
-    console.log('C')
-
-    await toppingsChoice()
-    console.log('D')
-    console.log('E')
-}
-
-kitchen()
-
-console.log('Doing the dishes')
-console.log('Cleaning the tables')
-console.log('Taking others orders')
